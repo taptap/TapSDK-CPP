d5dec60d40a79f99cd0cfdf626b01ae72b4f71e5
swift_gan <939543405@qq.com>
2023-08-17 11:12:10 +0800

    build: downgrade ios target

--- scripts/build-ios-sim.sh
+++ scripts/build-ios-sim.sh
@@ -1,5 +1,5 @@
 cd ../ios || exit
 mkdir build-sim
 cd build-sim || exit
-cmake ../../ -G Ninja -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DDEPLOYMENT_TARGET=14.0 -DCMAKE_OSX_ARCHITECTURES="x86_64" -DENABLE_BITCODE=0 -DPLATFORM=SIMULATOR64 -DCSHARP_BINDING=./swig
+cmake ../../ -G Ninja -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DDEPLOYMENT_TARGET=10.0 -DCMAKE_OSX_ARCHITECTURES="x86_64" -DENABLE_BITCODE=0 -DPLATFORM=SIMULATOR64 -DCSHARP_BINDING=./swig
 ninja sdk_combine -j8
